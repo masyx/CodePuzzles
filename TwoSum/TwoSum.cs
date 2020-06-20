ac7ed3987b11246d2689f867f4ecada40be79e6d
Sergey <rfmatveev@gmail.com>
2020-06-20 12:03:00 -0500

    Commit 06/20/20 TwoSum - Brute Force solution

---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class TwoSum
+    {
+        public static void Main(string[] args)
+        {
+            var result = TwoSumBruteForce(new int[] { 2, 11, 7, 15 }, 9);
+            Console.WriteLine($"{result[0]}, {result[1]}");
+        }
+
+        public static int[] TwoSumBruteForce(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return new int[] { i, j };
+                    
+                }
+            }
+            throw new ArgumentNullException("No two sum solution");
+        }
+    }
+}
