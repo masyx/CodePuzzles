5b3e4de5ba5269f591376b9de5c50729ccb73137
Sergey M <rfmatveev@gmail.com>
2021-03-25 23:32:12 -0400

    Number of coins problem cs50

---
+++ NumberOfCoinsCS50/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumberOfCoinsCS50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dollars;
+            int coins = 0;
+
+            do
+            {
+                Console.Write("Change: ");
+                dollars = decimal.Parse(Console.ReadLine());
+            }
+            while (dollars <= 0);
+
+            int cents = (int)Math.Round(dollars * 100);
+
+            int[] denominations = { 25, 10, 5, 1 };
+            
+            //int size = sizeof(denominations) / sizeof(int); c language solution
+
+            for (int i = 0; i < denominations.Length; i++)
+            {
+                coins += cents / denominations[i];
+                cents %= denominations[i];
+            }
+
+            Console.WriteLine($"{coins}");
+        }
+    }
+}
