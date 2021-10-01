66e127cb6a12bdd1ade679a26de461078afbfe35
Sergey Matveev <rfmatveev@gmail.com>
2021-09-30 23:14:31 -0500

    LengthOfLastWord

---
+++ LengthOfLastWord/LengthOfLastWordProgram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LengthOfLastWord
+{
+    class LengthOfLastWordProgram
+    {
+        static void Main(string[] args)
+        {
+            //
+            //"   fly me   to   the moon "
+            var i = LengthOfLastWordCPP("   fly me   to   the WORLD  ");
+            Console.WriteLine($"Length of last word is {i}");
+        }
+
+        public static int LengthOfLastWord(string input)
+        {
+            int lastWordLength = 0;
+            int currentWordLength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Equals(' '))
+                {
+                    currentWordLength = 0;
+                } 
+                else
+                {
+                    currentWordLength++;
+                    lastWordLength = currentWordLength;
+                }
+            }
+
+            return lastWordLength;      
+        }
+
+        public static int LengthOfLastWordCPP(string s)
+        {
+            int lengh = 0, tail = s.Length - 1;
+
+            while (s[tail] >= 0 && s[tail] == ' ')
+                tail--;
+            while (s[tail] != ' ')
+            {
+                lengh++;
+                tail--;
+            }
+            return lengh;
+        }
+    }
+}
