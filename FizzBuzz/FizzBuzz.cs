a2382ef4787d4743c18574b3fa5041876e8537b1
Sergey Matveev <rfmatveev@gmail.com>
2021-09-30 21:51:22 -0500

    FizzBuzz edit

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -56,7 +56,7 @@ namespace FizzBuzz
 
             for(int i = 1; i < 101; i++)
             {
-                if (i % 5 == 0 && i % 3 == 0)
+                if (i % 15 == 0)
                     result.Add("FizzBuzz");
                 else if (i % 3 == 0)
                     result.Add("Fizz");
