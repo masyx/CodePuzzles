74259701533dd4a61560e4f328484df7213f647d
Sergey Matveev <rfmatveev@gmail.com>
2022-08-12 22:14:33 -0500

    AverageOfSubarrays O(n*k) solution

---
+++ AverageOfSubarrays/AveragesOfSubarrays.cs
@@ -0,0 +1,27 @@
+﻿namespace AverageOfSubarrays
+{
+    internal class AveragesOfSubarrays
+    {
+        static void Main(string[] args)
+        {
+            var arr = new List<float>(){ 1, 3, 2, 6, -1, 4, 1, 8, 2 };
+            var result = AveragesOfSubarraysMethod(5, arr);
+            Console.WriteLine(result);
+        }
+
+        public static List<float> AveragesOfSubarraysMethod(int K, List<float> array)
+        {
+            var result = new List<float>();
+            for(int i = 0; i <= array.Count - K; i++)
+            {
+                float sum = 0;
+                for(int j = i; j < i + K; j++)
+                {
+                    sum += array[j];
+                }
+                result.Add(sum/K);
+            }
+            return result;
+        }
+    }
+}
