c0df346abbbc3a321514679bbd0bedf4e9f4de8c
Sergey Matveev <rfmatveev@gmail.com>
2022-08-13 16:31:42 -0500

    Revert "Revert "AveragesOfSubarray time complexity for brute force solution""
    This reverts commit 00a785b06f56aadcf501a00e39a33c556dc4c51b.

---
+++ TRY/TRY.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TRY
+{
+    class TRY
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)'A');
+            Console.WriteLine(char.ConvertFromUtf32('A' + 1));
+        }
+    }
+}
