5ca919f7f064009064eda1b0182a0c043d43b8a0
Sergey Matveev <rfmatveev@gmail.com>
2023-01-28 18:37:57 -0600

    FirstNonRepeatingInteger solution with dictionary

---
+++ FirstNonRepeatingInteger/FirstNonRepeatingInteger.cs
@@ -0,0 +1,35 @@
+﻿namespace FirstNonRepeatingInteger
+{
+    internal class FirstNonRepeatingInteger
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 1, 2, 3, 4 };
+            Console.WriteLine(FirstNonRepeatingIntegerMethod(list));
+        }
+
+        private static int FirstNonRepeatingIntegerMethod(List<int> list)
+        {
+            var counts = new Dictionary<int, int>();
+            foreach (var item in list)
+            {
+                if (counts.TryGetValue(item, out var value))
+                {
+                    counts[item] += 1;
+                }
+                else
+                {
+                    counts[item] = 1;
+                }
+            }
+
+            foreach (var item in counts)
+            {
+                if (item.Value == 1)
+                    return item.Key;
+            }
+
+            return -1;
+        }
+    }
+}
