7483f6b253f189824c6cb6755449b2854b5e639a
Sergey Matveev <rfmatveev@gmail.com>
2023-02-11 22:06:54 -0600

    Created LeetCode and LeetCode.Test projects and solved RotateArray problem

---
+++ LeetCode.Test/RotateArrayTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture]
+    public class RotateArrayTest
+    {
+
+        [Test]
+        public void RotateArrayTest_1()
+        {
+            var array = new int[] { 1,2,3,4 };
+
+            var rotator = new LeetCode.RotateArray();
+            var rotatedArray = rotator.RotateArrayMethod(array, 2);
+
+            Assert.AreEqual(1, rotatedArray[2]);
+            
+        }
+
+        [Test]
+        public void RotateArrayTest_2()
+        {
+            var array = new int[] { };
+
+            var rotator = new LeetCode.RotateArray();
+            var rotatedArray = rotator.RotateArrayMethod(array, 2);
+
+            Assert.IsEmpty(rotatedArray);
+
+        }
+
+    }
+}
---
+++ LeetCode/RotateArray.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode
+{
+    public class RotateArray
+    {
+        public int[] RotateArrayMethod(int[] array, int k)
+        {
+            int[] result = new int[array.Length];
+            for(int i = 0; i < array.Length; i++)
+            {
+                result[(i + k) % array.Length] = array[i];
+            }
+            return result;
+        }
+
+    }
+}
