1f0f6c31590164e7696bf4e8c0271f82f15e690f
Sergey Matveev <rfmatveev@gmail.com>
2023-02-13 23:46:56 -0600

    FindMedianOfTwoSortedArrays solution

---
+++ LeetCode.Test/FindMedianOfTwoArraysTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture]
+    internal class FindMedianOfTwoArraysTest
+    {
+        [Test]
+        public void FindMedianOfTwoArraysTest_1()
+        {
+            var arr1 = new int[] { 1, 2, 3};
+            var arr2 = new int[] { 4 };
+
+            var median = FindMedianOfTwoArrays.MedianOfTwoArrays(arr1, arr2);
+
+            Assert.AreEqual(2.5, median);
+        }
+
+        [Test]
+        public void FindMedianOfTwoArraysTest_2()
+        {
+            var arr1 = new int[] { 1, 2, 3 };
+            var arr2 = new int[] { 4, 5};
+
+            var median = FindMedianOfTwoArrays.MedianOfTwoArrays(arr1, arr2);
+
+            Assert.AreEqual(3, median);
+        }
+
+        [Test]
+        public void FindMedianOfTwoArraysTest_EmptyLists()
+        {
+            var arr1 = Array.Empty<int>();
+            var arr2 = new int[0];
+
+            Assert.Throws<ArgumentException>(
+                () => FindMedianOfTwoArrays.MedianOfTwoArrays(arr1, arr2), "Both arrays are empty.");
+        }
+    }
+}
---
+++ LeetCode/FindMedianOfTwoArrays.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class FindMedianOfTwoArrays
+    {
+        // O(n) time | O(n + m) space
+        public static double MedianOfTwoArrays(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length == 0 && arr2.Length == 0)
+                throw new ArgumentException("Both arrays are empty.");
+
+            var merged = new int[arr1.Length + arr2.Length];
+            int k = 0, i = 0, j = 0;
+            while (i != arr1.Length || j != arr2.Length)
+            {
+                if (j == arr2.Length || (i < arr1.Length && arr1[i] < arr2[j]))
+                {
+                    merged[k++] = arr1[i++];
+                }
+                else
+                {
+                    merged[k++] = arr2[j++];
+                }
+            }
+            int medianIdx = (merged.Length - 1) / 2;
+            if(merged.Length % 2 == 0)
+            {
+                return (merged[medianIdx] + merged[medianIdx + 1]) / 2.0;
+            }
+            else
+            {
+                return merged[medianIdx];
+            }
+        }
+    }
+}
