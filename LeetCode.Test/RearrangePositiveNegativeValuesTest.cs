3e689477249234aed78fe8c5c0a089045a85ac93
Sergey Matveev <rfmatveev@gmail.com>
2023-02-16 17:43:08 -0600

    RearrangePositiveNegativeValues solution

---
+++ LeetCode.Test/RearrangePositiveNegativeValuesTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture]
+    internal class RearrangePositiveNegativeValuesTest
+    {
+        [Test]
+        public void RearrangeTest()
+        {
+            var ints = new List<int> { 2, 6, 3, -1, -8, 5, -9};
+
+            RearrangePositiveNegativeValues.Rearrange(ints);
+
+            Assert.AreEqual(-9, ints[2]);
+            Assert.AreEqual(2, ints[3]);
+        }
+    }
+}
---
+++ LeetCode/RearrangePositiveNegativeValues.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RearrangePositiveNegativeValues
+    {
+        public static void Rearrange(List<int> collection)
+        {
+            int lastIdx = 0;
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (collection[i] < 0)
+                {
+                    var temp = collection[lastIdx];
+                    collection[lastIdx] = collection[i];
+                    collection[i] = temp;
+                    lastIdx++;
+                }
+            }
+        }
+    }
+}
