33650dff0bfdd67fe8c886c712cccfa792d833f3
Sergey Matveev <rfmatveev@gmail.com>
2023-02-17 21:43:36 -0600

    RearrangeMixMin my solution

---
+++ LeetCode.Test/RearrangeMaxMinTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture]
+    internal class RearrangeMaxMinTest
+    {
+        [Test]
+        public void RearrangeToMaxMinTest_1()
+        {
+            var lst = new List<int> { 1, 2, 3, 4, 5 };
+            var resultLst = RearrangeMaxMin.RearrangeToMaxMin(lst);
+
+            Assert.AreEqual(2, resultLst[3]);
+        }
+
+        [Test]
+        public void RearrangeToMaxMinTest_2()
+        {
+            var lst = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var resultLst = RearrangeMaxMin.RearrangeToMaxMin(lst);
+
+            Assert.AreEqual(3, resultLst[5]);
+        }
+
+        [Test]
+        public void RearrangeToMaxMinTestEmptyList()
+        {
+            var lst = new List<int>();
+            var resultLst = RearrangeMaxMin.RearrangeToMaxMin(lst);
+
+            Assert.IsEmpty(resultLst);
+        }
+    }
+}
---
+++ LeetCode/RearrangeMaxMin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class RearrangeMaxMin
+    {
+        public static List<int> RearrangeToMaxMin(List<int> list)
+        {
+            if (list.Count == 0)
+                return list;
+
+            var result = new List<int>(list.Count);
+            var start = 0;
+            var end = list.Count - 1;
+            while(start < end)
+            {
+                result.Add(list[end--]);
+                result.Add(list[start++]);
+            }
+
+            if (list.Count % 2 == 1)
+                result.Add(list[(list.Count - 1) / 2]);
+
+            return result;
+        }
+    }
+}
