58f16d4ae859082426ea39735113b6d6907d7b90
Sergey Matveev <rfmatveev@gmail.com>
2023-09-23 22:35:55 -0500

    TwoSum problem

---
+++ LeetCode.Test/TwoSumTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test
+{
+    [TestFixture]
+    internal class TwoSumTest
+    {
+        [Test]
+        public void TwoSumTestHappyPath()
+        {
+            var list = new List<int> { 4, 5, 2, 10 };
+            var result = TwoSum.TwoSumHashMap(list, 14);
+            Assert.AreEqual(3, result[0]);
+            Assert.AreEqual(0, result[1]);
+        }
+    }
+}
---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class TwoSum
+    {
+        public static List<int> TwoSumHashMap(List<int> nums, int targetSum)
+        {
+            Dictionary<int, int> seen = new();
+            
+            for(int i = 0; i < nums.Count; i++)
+            {
+                var complement = targetSum - nums[i];
+                if (seen.TryGetValue(complement, out int index))
+                {
+                    return new List<int> { i, index };
+                }
+                seen[nums[i]] = i;
+            }
+
+            return new List<int>();
+        }
+    }
+}
